583c219bf90ff3e8e3af3e50547b0a1e02bfae24
tlcDataScience <102406967+tlcDataScience@users.noreply.github.com>
2023-12-28 14:54:17 +0000

    1

--- app.py
+++ app.py
@@ -5,17 +5,35 @@ app = Flask(__name__)
 JOBS = [
     {
         'id': 1,
-        'title': 'A'       
+        'title': 'Data Analyst',
+        'location': 'Banglore',
+        'salary': '100000'
     },
     {
         'id': 2,
-        'title': 'B'       
+        'title': 'Data Scientist',
+        'location': 'Dehli',
+        'salary': '1000000' 
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+        'salary': '200000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'USA',  
     }
 ]
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', jobs=JOBS)
+    return render_template('home.html', 
+                           jobs=JOBS,
+                           companyname ="Jovian Careers"
+                            )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
